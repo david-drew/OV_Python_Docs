aae6ac5fee106e3279c7aab62942d7f770d1692a
david-drew <david.drewelow@gmail.com>
2021-10-06 18:40:42 -0700

    Create AUTO3.py

---
+++ openvino/docs/snippets/AUTO3.py
@@ -0,0 +1,10 @@
+  from openvino.inference_engine import IECore, StatusCode
+
+  # Init the Inference Engine Core
+  ie = IECore()
+
+  # Read a network in IR or ONNX format
+  net = ie.read_network(model="sample.xml")
+
+  # Load the network to the AUTO device
+  exec_net = ie.load_network(network=net, device_name="AUTO")
